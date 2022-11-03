02c794219fdf34873f8f8f091104831ed85d28e6
Edra Prasetio <67980359+edraprasetio@users.noreply.github.com>
2022-11-03 11:27:14 -0700

    Update urls.py

--- sma/urls.py
+++ sma/urls.py
@@ -5,6 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
+    dawdojs aposjfdans aosdfnjoasj
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
